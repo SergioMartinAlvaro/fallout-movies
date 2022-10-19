34aab1bb1b53af19cc05617ab77fc425f06f17ff
SergioMartinAlvaro <sergiomartin@overture.life>
2022-10-19 16:07:46 +0200

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AppProvider } from '@/providers/app';
 import { AppRoutes } from '@/routes';
+import './index.css';
 
 function App() {
   return (
--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -1,8 +1,7 @@
 import { ContentLayout } from '@/components/Layout';
-
 export const Home = () => {
   return (
-    <ContentLayout title="Dashboard">
+    <ContentLayout title="Fallout Cinema">
       <h1 className="text-xl mt-2">
         Welcome <b>User</b>
       </h1>
